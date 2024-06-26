2b4e030fb7c4b7baa39686855f7c9ae4ec650744
Alexander <82063585+alcheremnyh@users.noreply.github.com>
2024-06-26 09:02:04 +0300

    users list post

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -7,7 +7,7 @@ router.post('/register', controller.register)
 router.get('/rules', controller.rules_get)
 router.post('/rules-set', controller.rules_set)
 router.get('/gen-password', controller.gen_password)
-router.get('/list', controller.list_get)
+router.post('/list', controller.list_get)
 router.post('/status-set', controller.status_set)
 router.get('/info', controller.info_get)
 
--- db_bak/transactions/get_transactions.sql
+++ db_bak/transactions/get_transactions.sql
@@ -15,10 +15,12 @@ DECLARE
 	v_user_id BIGINT;
 	v_role BIGINT;
 	v_token TEXT;
+	v_created TIMESTAMP WITH TIME ZONE;
 BEGIN
 	SELECT ul.id, ul.role FROM users.list ul WHERE ul.token = p_token INTO v_user_id, v_role;
 	IF v_role = 4 THEN
-		RETURN QUERY SELECT * FROM transactions.list tl WHERE tl.user_id_from = v_user_id OR tl.user_id_to = v_user_id ORDER BY id DESC;
+		select us.start from users.shifts us where us.user_id = v_user_id and us.complete = true ORDER BY us.id DESC LIMIT 1 INTO v_created;
+		RETURN QUERY SELECT * FROM transactions.list tl WHERE (tl.user_id_from = v_user_id OR tl.user_id_to = v_user_id) AND tl.created>v_created ORDER BY tl.id DESC;
 	ELSE
 		RAISE EXCEPTION 'wrong role';
 	END IF;
